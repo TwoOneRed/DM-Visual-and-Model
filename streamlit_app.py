5adab110989bf5deda443d3afdd40959c7d9d18f
TwoOneRed <102686099+TwoOneRed@users.noreply.github.com>
2023-01-10 20:33:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,30 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('Data_Cleaned.csv')
+st.title('Data Mining Project)
+st.text('1191100280  Alvin Fong Weng Yew')
+st.text('1191100281  Tan Sin Zhung')
+st.text('1191100292  Leong Yi Hong')
 
+st.title('Cleaned Datasets')
+df = pd.read_csv('Data_Cleaned.csv')
 st.dataframe(df)
 #st.write(df['Date'])
+
+#QUESTION 1
+st.title('Question 1')
+         
+q1 = df[['Race','TotalSpent_RM']]
+q1 = q1.dropna()
+q1['TotalSpent_RM'] = q1['TotalSpent_RM'].astype(int)
+
+malay = q1[q1['Race']=='malay']['TotalSpent_RM']
+indian = q1[q1['Race']=='indian']['TotalSpent_RM']
+chinese = q1[q1['Race']=='chinese']['TotalSpent_RM']
+
+q1 = [malay, indian, chinese]
+q1plt =plt.boxplot(q1,labels=['Malay','Indian','Chinese'])
+q1plt = plt.xlabel('Race')
+q1plt = plt.ylabel('Money Spent')
+         
+st.pyplot(q1plt)
