c07df6846fe6f3f852ce38649cae66bd961ad328
TwoOneRed <leong_2001@hotmail.com>
2023-01-11 12:56:18 +0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,9 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 
+from mlxtend.frequent_patterns import fpgrowth
+from mlxtend.frequent_patterns import association_rules
+
 df = pd.read_csv('Data_Cleaned.csv')
 dataset = pd.read_csv('laundry.csv')
 weather = pd.read_csv('weather.csv')
@@ -35,7 +38,7 @@ df_encode = df_encode.apply(LabelEncoder().fit_transform)
 
 st.title('Data Mining Project')
 st.header('Member')
-member = pd.DataFrame({"ID":['1191100280','1191100281','1191100292'],"Name":['Alvin Fong Weng Yew','Tan Sin Zhung','Leong Yi Hong'], "Phone Number":['011-2029 5617','011-366 1060','011-7289 2995']})
+member = pd.DataFrame({"ID":['1191100280','1191100281','1191100292'],"Name":['Alvin Fong Weng Yew','Tan Sin Zhung','Leong Yi Hong'], "Phone Number":['011-20295617','011-3661060','011-72892995']})
 st.dataframe(member)
 
 st.header('Laundry Datasets')
@@ -48,8 +51,9 @@ st.header('Cleaned Datasets')
 st.dataframe(df)
 
 #QUESTION 1
-st.header('Question 1')
-st.subheader('Is there a significant difference between Race (Malay, Indian, Chinese, Foreigner) and the sales of drink in the laundry store?')
+st.header('Exploratory Data Analysis')
+st.subheader('Question 1')
+st.text('Is there a significant difference between Race (Malay, Indian, Chinese, Foreigner) and the sales of drink in the laundry store?')
 
 q1 = df[['Race','buyDrink']]
 
@@ -64,8 +68,8 @@ st.pyplot(q1plt)
 
 
 #QUESTION 2
-st.header('Question 2')
-st.subheader('Do basket size and family with kids influence the number of people buying drinks?')
+st.subheader('Question 2')
+st.text('Do basket size and family with kids influence the number of people buying drinks?')
 
 q2 = df[['Basket_Size', 'With_Kids', 'buyDrink']]
 
@@ -98,8 +102,8 @@ st.dataframe(sm.stats.anova_lm(model, typ=2))
 
 #QUESTION 3
 
-#st.header('Question 3')
-#st.subheader('Any correlations between weather information and number of people buying drinks?')
+#st.subheader('Question 3')
+#st.text('Any correlations between weather information and number of people buying drinks?')
 
 
 #q3 = df[['temp', 'humidity', 'windspeed','cloudcover', 'visibility', 'buyDrink']].reset_index(drop=True)
@@ -110,8 +114,8 @@ st.dataframe(sm.stats.anova_lm(model, typ=2))
 #st.pyplot(q3plt)
 
 #QUESTION 4
-st.header('Question 4')
-st.subheader('Are there difference in average total spent (RM) in laundry shops between each of the age groups?')
+st.subheader('Question 4')
+st.text('Are there difference in average total spent (RM) in laundry shops between each of the age groups?')
 
 q4 = df[['Age_Range','TotalSpent_RM']]
 
@@ -129,7 +133,24 @@ for i in ax.containers:
 
 st.pyplot(q4plt)
 
-st.header('Feature Selection')
+st.subheader('Question 5')
+st.text('ARM')
+q6 = df[['Race','Gender','Body_Size','With_Kids','Kids_Category','Basket_colour','Attire','Shirt_Colour','shirt_type','Pants_Colour','pants_type','Wash_Item','Day','Time_Of_The_Day','Spectacles']]
+
+oneh = pd.get_dummies(q6)
+
+# Find frequent item sets using the FP-growth algorithm
+frequent_item_sets = fpgrowth(oneh, min_support=0.10, use_colnames=True)
+
+# Compute association rules
+rules = association_rules(frequent_item_sets, metric='confidence', min_threshold=0.3)
+
+# Display the association rules
+rules
+
+
+
+st.header('PART 2. Feature Selection')
 st.subheader('BORUTA Features')
 # Feature selection using BORUTA
 rf = RandomForestClassifier(n_jobs=-1, class_weight="balanced_subsample", max_depth=5)
@@ -161,5 +182,46 @@ st.text('BORUTA Bottom 10 Features')
 st.dataframe(boruta_score.tail(10))
 
 st.subheader('RFE Top 10 Features')
+st.text('RFE Top 10 Features')
+st.dataframe(boruta_score.head(10))
 
-st.header('Feature Comparison')
+st.text('RFE Bottom 10 Features')
+st.dataframe(boruta_score.tail(10))
+
+st.subheader('Feature Comparison')
+
+
+st.header('PART 3 Model Construction and Comparison')
+st.subheader('Classification For Naive Bayes')
+
+st.text("Naive Bayes Top 5 Features")
+
+st.text("Naive Bayes Top 10 Features")
+
+st.text("Compare Naive Bayes by Features")
+
+st.text("SMOTE comparison for Naive Bayes")
+
+
+st.subheader('Classification For XGBoost')
+
+st.text("XGBoost Top 5 Features")
+
+st.text("XGBoost Top 10 Features")
+
+st.text("Compare XGBoost by Features")
+
+st.text("SMOTE comparison for XGBoost")
+
+st.subheader('Ensemble')
+
+st.subheader('Hyperparameter')
+
+st.subheader('Regression for Linear Regression')
+
+st.subheader('Regression for Logistic Regression')
+
+st.subheader()
+st.subheader()
+st.subheader()
+
