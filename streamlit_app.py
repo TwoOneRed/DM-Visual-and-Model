e8259b950d1ee0e324e8aac45df4bc1195a6adea
TwoOneRed <leong_2001@hotmail.com>
2023-01-11 12:35:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 from statsmodels.formula.api import ols
 import statsmodels.api as sm
